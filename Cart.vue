f17d705bc5153bc22245e1ea71d8de8528dab0e6
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-10 18:18:18 +0530

    Update Cart.vue

--- Cart.vue
+++ Cart.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
-    
-    
-    
     <div class="d-grid gap-2">
             <button class="btn btn-primary" type="button" @click="productBtnClicked">Go To Products</button>
     </div>
-    <div v-for="product in cart" :key="product.product_name" > 
-        <div class="card" style="width: 18rem;">
-        <img src="" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title">{{product.product_name}}</h5>
-            <p class="card-text">{{product.currency_sign}}{{product.cost}}</p>
-        </div>
-        </div>
-    </div>
-      
-      
-      
+    <h1 v-if="cart.length==0" class="container">No Items in Cart</h1>
 
+<!-- table -->
+<div v-else>
+
+    <table class="table table-striped table-hover">
+    <thead>
+        <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Price</th>
+        <th scope="col">Quantity</th>
+        <th scope="col">Sub total</th>
+        </tr>
+    </thead>
+    <tbody v-for="product in cart" :key="product.product_name" >
+        <tr>
+        <th scope="row">{{product.product_name}}</th>
+        <td>{{product.currency_sign}}{{product.cost}}</td>
+        <td>{{product.qty}}x</td>
+        <td>{{product.cost*product.qty}}</td>
+        </tr>
+        
+    </tbody>
+    </table>
+    <h1 >SubTotal:{{Total}}</h1>
+</div>
   </div>
 </template>
 
@@ -27,9 +37,11 @@ export default {
   name: 'Cart',
   props: {
       cart:Array,
+      Total:String
   },
   data(){
       return {
+          subTotal:0,
       }
   },
   methods:{
