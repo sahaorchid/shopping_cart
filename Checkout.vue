28784b7a76a387b10c97cec1aca79080f2e6c19f
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-11 11:39:11 +0530

    Add files via upload

---
+++ Checkout.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+<form class="row g-3" @submit="dataSubmit">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Name</label>
+    <input type="text" class="form-control" id="inputEmail4" name="name" v-model="name">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Email</label>
+    <input type="Email" class="form-control" id="inputPassword4" name="email" v-model="email">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input type="text" class="form-control" id="inputAddress" name="address" v-model="address" >
+  </div>
+
+
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary" >Place Order</button>
+  </div>
+</form>
+      
+      
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkout',
+  props: {
+  },
+  data(){
+      return {
+          name:null,
+          email:null,
+          address:null
+          
+      }
+  },
+  methods:{
+      dataSubmit(e){
+          e.preventDefault();
+          alert(`Name:${this.name},Email:${this.email},Address:${this.address}`)
+      }
+
+
+  },
+  
+}
+</script>
+<style>
+
+</style>
+
+
