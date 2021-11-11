f2b65989b80f8a53e4e6c066e62b7b1a70c9765b
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-11 18:06:12 +0530

    Update Success.vue

--- Success.vue
+++ Success.vue
@@ -1,68 +1,60 @@
 <template>
-  <div>
-     <h1>{{cart}}</h1>
-     <h2>{{user_data}}</h2>
+  <div id="div">
      <h1>product info</h1>
-
+     <div v-for="product in cart" :key="product.product_name">
+        <p scope="row">{{product.product_name}}</p>
+        <p>{{product.currency_sign}}{{product.cost}}</p>
+        <p>{{product.qty}}x</p>
+        <p id="name">{{product.cost*product.qty}}</p>
+     </div>
+     <h1>User Details</h1>
+     <p>{{user_data.Name}}</p>
+     <p>{{user_data.Email}}</p>
+     <p>{{user_data.Address}}</p>
   </div>
 </template>
 
 <script>
-//confetti 
-// const confetti = require('canvas-confetti');
-// var myCanvas = document.createElement('canvas');
-// document.body.appendChild(myCanvas);
-
-// var myConfetti = confetti.create(myCanvas, {
-//   resize: true,
-//   useWorker: true
-// });
-// myConfetti({
-//   particleCount: 100,
-//   spread: 160
-// });
+import confetti from 'canvas-confetti'
+let myCanvas = document.createElement('canvas');
 export default {
   name: 'Success',
   props: {
-      cart:Array,
-      user_data:Array
+      cart: Array,
+      user_data: Object
   },
   data(){
       return {
-          name:null,
-          email:null,
-          address:null
-          
+          name: null,
+          email: null,
+          address: null   
       }
   },
   created:function(){
-      const confetti = require('canvas-confetti');
-var myCanvas = document.createElement('canvas');
-document.body.appendChild(myCanvas);
-
-var myConfetti = confetti.create(myCanvas, {
-  resize: true,
-  useWorker: true
-});
-myConfetti({
-  particleCount: 100,
-  spread: 160
-});
-
+    if(myCanvas){
+        let canvas=document.getElementById("canvas")
+        let myConfetti = confetti.create(canvas, {
+        resize: true,
+        useWorker: true
+        });
+        myConfetti({
+        particleCount: 100,
+        spread: 10
+        });
+    }
+    else{
+        myCanvas.setAttribute('id','canvas')
+        document.body.appendChild(myCanvas);
+    }
   },
   methods:{
       dataSubmit(e){
           e.preventDefault();
           alert(`Name:${this.name},Email:${this.email},Address:${this.address}`)
       }
-
-
-  },
-  
+  },  
 }
 </script>
-<style>
 
-</style>
 
 
