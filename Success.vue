a2621d6fe745738162b07334f34889300874efb9
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-11 13:40:48 +0530

    Add files via upload

---
+++ Success.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+     <h1>{{cart}}</h1>
+     <h2>{{user_data}}</h2>
+     <h1>product info</h1>
+
+  </div>
+</template>
+
+<script>
+//confetti 
+// const confetti = require('canvas-confetti');
+// var myCanvas = document.createElement('canvas');
+// document.body.appendChild(myCanvas);
+
+// var myConfetti = confetti.create(myCanvas, {
+//   resize: true,
+//   useWorker: true
+// });
+// myConfetti({
+//   particleCount: 100,
+//   spread: 160
+// });
+export default {
+  name: 'Success',
+  props: {
+      cart:Array,
+      user_data:Array
+  },
+  data(){
+      return {
+          name:null,
+          email:null,
+          address:null
+          
+      }
+  },
+  created:function(){
+      const confetti = require('canvas-confetti');
+var myCanvas = document.createElement('canvas');
+document.body.appendChild(myCanvas);
+
+var myConfetti = confetti.create(myCanvas, {
+  resize: true,
+  useWorker: true
+});
+myConfetti({
+  particleCount: 100,
+  spread: 160
+});
+
+  },
+  methods:{
+      dataSubmit(e){
+          e.preventDefault();
+          alert(`Name:${this.name},Email:${this.email},Address:${this.address}`)
+      }
+
+
+  },
+  
+}
+</script>
+<style>
+
+</style>
+
+
