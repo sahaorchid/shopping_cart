012d3866999e3379e5cbe6ebc201b12e93fb5a63
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-11 18:02:18 +0530

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,15 +1,9 @@
 <template>
-    
-    
-    <template>
-    
-    
-    
     <div>
-    <Listing v-if="currentPage=='listing'" :cart="totalItem" :products="products"  @addCart="addToCart($event)" @goToPage="goToPage($event)" @click="total"/>
-    <Cart  v-if="currentPage=='cart'" :cart="cart" @goToPage="goToPage($event)" :Total="subTotal"/>
-    <Checkout v-if="currentPage=='checkout'" @goToPage="goToPage($event)" @userData="userData($event)"/>
-    <Success v-if="currentPage=='success'"  @goToPage="goToPage($event)" :cart="cart"  :user_data="user_data"/>
+      <Listing v-if="currentPage=='listing'" :cart="totalItem" :products="products"  @addCart="addToCart($event)" @goToPage="goToPage($event)" @click="total"/>
+      <Cart  v-if="currentPage=='cart'" :cart="cart" @goToPage="goToPage($event)" :Total="subTotal"/>
+      <Checkout v-if="currentPage=='checkout'" @goToPage="goToPage($event)" @userData="userData($event)"/>
+      <Success v-if="currentPage=='success'"  @goToPage="goToPage($event)" :cart="cart"  :user_data="user_data"/>
     </div>
 </template>
 
@@ -47,26 +41,26 @@ export default {
           currency_sign: '$',
         },
       ],
-      cart:[],
-      id:0,
-      totalItem:0,
-      subTotal:0,
-      user_data:[]
+      cart: [],
+      id: 0,
+      totalItem: 0,
+      subTotal: 0,
+      user_data: {}
     }
   },
   methods:{
     addToCart(product){
-      let item=(this.cart.find((item)=>{
-       return  item.product_name==product.product_name
+      let item = (this.cart.find((item)=>{
+       return  item.product_name == product.product_name
         }))
       if(item){
-        this.totalItem+=1
-        item.qty+=1
+        this.totalItem+= 1
+        item.qty+= 1
       }
       else{
-        this.id+=1
-        this.totalItem+=1
-        this.cart.push({id:this.id,product_name:product.product_name,cost:product.cost,qty:1,currency_sign:product.currency_sign})
+        this.id+= 1
+        this.totalItem+= 1
+        this.cart.push({...product,id:this.id,qty:1})
       }
     },
     goToPage(value){
@@ -74,103 +68,12 @@ export default {
     },
     total(){
       this.subTotal=this.cart.reduce((total,item)=>{
-        return total+=(item.cost*item.qty)
+        return total+= (item.cost*item.qty)
       },0)
-
     },
     userData(data){
-      console.log(data)
-      this.user_data.push(data)
-
+      this.user_data = (data)
     }
-
-
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
-    <div>
-    <Listing v-if="currentPage=='listing'" :cart="totalItem" :products="products"  @addCart="addToCart($event)" @goToPage="goToPage($event)" @click="total"/>
-    <Cart  v-if="currentPage=='cart'" :cart="cart" @goToPage="goToPage($event)" :Total="subTotal"/>
-    <Checkout v-if="currentPage=='checkout'"/>
-    </div>
-</template>
-
-<script>
-import Listing from './components/Listing.vue'
-import Cart from './components/Cart.vue'
-import Checkout from './components/Checkout.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Listing,
-    Cart,
-    Checkout
-  },
-  data(){
-    return {
-      currentPage:"listing",
-      products:[
-        {
-          product_name: 'Laptop',
-          cost: 450,
-          currency_sign: '$',
-        },
-        {
-          product_name: 'Mobiles',
-          cost: 300,
-          currency_sign: '$',
-        },
-        {
-          product_name: 'Desktops',
-          cost: 800,
-          currency_sign: '$',
-        },
-      ],
-      cart:[],
-      id:0,
-      totalItem:0,
-      subTotal:0,
-    }
-  },
-  methods:{
-    addToCart(product){
-      let item=(this.cart.find((item)=>{
-       return  item.product_name==product.product_name
-        }))
-      if(item){
-        this.totalItem+=1
-        item.qty+=1
-      }
-      else{
-        this.id+=1
-        this.totalItem+=1
-        this.cart.push({id:this.id,product_name:product.product_name,cost:product.cost,qty:1,currency_sign:product.currency_sign})
-      }
-    },
-    goToPage(value){
-      this.currentPage = value
-    },
-    total(){
-      this.subTotal=this.cart.reduce((total,item)=>{
-        return total+=(item.cost*item.qty)
-      },0)
-
-    }
-
-
   }
 }
 </script>
